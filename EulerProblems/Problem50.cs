1d3e1613cbd0118fa3eb9e86a043d12af1d88262
bonneyab <bonneyab@gmail.com>
2015-06-24 12:27:44 -0700

    Adding problem 50

---
+++ EulerProblems/Problem50.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EulerProblems.Helpers;
+
+namespace EulerProblems
+{
+//The prime 41, can be written as the sum of six consecutive primes:
+//41 = 2 + 3 + 5 + 7 + 11 + 13
+//This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+//The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+//Which prime, below one-million, can be written as the sum of the most consecutive primes?
+    public class Problem50
+    {
+        public int ConsecutivePrimeSum(int max)
+        {
+            var primes = PrimeNumberHelper.GetPrimeNumbersBelowNumber(max);
+
+            var allSums = new List<List<Tuple<int, int>>>();
+            int i = 0;
+            while (i < primes.Count)
+            {
+                var sums = new List<Tuple<int, int>>();
+                var next = 0;
+                var terms = 0;
+                foreach (var prime in primes.Skip(i))
+                {
+                    terms++;
+                    next += prime;
+                    if (next > max)
+                    {
+                        allSums.Add(sums);
+                        break;
+                    }
+                    sums.Add(new Tuple<int, int>(terms, next));
+                }
+                i++;
+            }
+            var combinedSums = allSums.SelectMany(x => x).OrderByDescending(x => x.Item1).ToList();
+
+            foreach (var sum in combinedSums)
+            {
+                if (primes.Contains(sum.Item2))
+                    return sum.Item2;
+            }
+            return -1;
+        }
+    }
+}
---
+++ EulerTests/Problem50Tests.cs
@@ -0,0 +1,36 @@
+﻿using EulerProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerTests
+{
+    [TestClass]
+    public class Problem50Tests
+    {
+        [TestMethod]
+        public void ConsecutivePrimeSum_100_returns41()
+        {
+            var problem = new Problem50();
+
+            var actual = problem.ConsecutivePrimeSum(100);
+            Assert.AreEqual(41, actual);
+        }
+
+        [TestMethod]
+        public void ConsecutivePrimeSum_1000_returns953()
+        {
+            var problem = new Problem50();
+
+            var actual = problem.ConsecutivePrimeSum(1000);
+            Assert.AreEqual(953, actual);
+        }
+
+        [TestMethod]
+        public void ConsecutivePrimeSum_1000000_returnsX()
+        {
+            var problem = new Problem50();
+
+            var actual = problem.ConsecutivePrimeSum(1000000);
+            Assert.AreEqual(953, actual);
+        }
+    }
+}
